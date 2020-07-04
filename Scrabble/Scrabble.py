941483b9ab229ea0c8f06f70d9b0c4830601d10a
duncanrout <65206033+duncanrout@users.noreply.github.com>
2020-07-04 14:36:00 -0500

    Update Scrabble.py

--- Scrabble/Scrabble.py
+++ Scrabble/Scrabble.py
@@ -21,16 +21,47 @@ grid.grid(sticky=N+S+E+W, column=0, row=7, columnspan=2)
 Grid.rowconfigure(frame, 7, weight=1)
 Grid.columnconfigure(frame, 0, weight=1)
 
-#creating board using grid
-for x in range(15):
-    for y in range(15):
-        btn = Button(frame, image = photoDW)
+#creating board using grid (15x15 with border)
+for x in range(17):
+    for y in range(17):
+        #Border
+        if(x == 0 or y == 0 or x == 16 or y == 16):
+            btn = Button(frame, image = photoBLANK)
+        #Triple Word
+        elif((x == 1 and y ==1) or (x == 8 and y == 1) or (x == 15 and y == 1) or
+             (x == 1 and y ==15) or (x == 8 and y == 15) or (x == 15 and y == 15) or
+             (x == 1 and y == 8) or (x == 15 and y == 8)):
+            btn = Button(frame, image = photoTW)
+        #Double Letter
+        elif((x == 4 and y ==1) or (x == 12 and y == 1) or (x == 7 and y == 3) or
+             (x == 9 and y ==3) or (x == 1 and y == 4) or (x == 8 and y == 4) or
+             (x == 15 and y == 4) or (x == 3 and y == 7)or (x == 7 and y == 7) or
+             (x == 9 and y == 7) or (x == 13 and y == 7) or(x == 4 and y == 8) or
+             (x == 12 and y == 8) or (x == 3 and y == 9) or (x == 7 and y == 9) or
+             (x == 9 and y == 9) or (x == 13 and y == 9) or (x == 1 and y == 12) or
+             (x == 8 and y == 12) or (x == 15 and y == 12) or (x == 7 and y == 13) or
+             (x == 9 and y == 13) or (x == 4 and y == 15) or (x == 12 and y == 15)):
+            btn = Button(frame, image = photoDL)
+        #Double Word
+        #--TODO--
+            
+
+        #Triple Letter
+        #--TODO--
+
+        #Star
+        #--TODO--
+
+            
+        #Remaining Tiles
+        else:
+            btn = Button(frame, image = photoBLANK)
         btn.grid(column=x, row=y, sticky=N+S+E+W)
 
-for x in range(15):
+for x in range(17):
   Grid.columnconfigure(frame, x, weight=2)
 
-for y in range(15):
+for y in range(17):
   Grid.rowconfigure(frame, y, weight=2)
 
 root.mainloop()
